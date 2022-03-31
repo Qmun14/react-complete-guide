2b6834d246149bbc41edff1c346a4a7121c11ce8
Qmun14 <mamunramdhan@gmail.com>
2022-03-31 23:22:54 +0700

    Adding normal JavaScript Logic to Components (insert date javascript API function -- .toLocaleString()

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,16 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
+    const month = props.date.toLocaleString('id-ID', {month : 'long'})
+    const year = props.date.toLocaleString('id-ID', {year : 'numeric'})
+    const day = props.date.toLocaleString('id-ID', {day : '2-digit'})
   return (
     <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
+      <div>
+          <div>{month}</div>
+          <div>{year}</div>
+          <div>{day}</div>
+      </div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>Rp. {props.amount}</div>
