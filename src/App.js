20b71bad639beb0c41aa8ecea73dd6d5a55af0bf
Qmun14 <mamunramdhan@gmail.com>
2022-06-28 20:15:45 +0700

    Finishing topic about (splitting components into multiple components using *props*)

--- src/App.js
+++ src/App.js
@@ -1,51 +1,32 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expense = [{
     title: "Car Insurance",
     amount: 1250000,
-    date: new Date(2022, 2, 31)
+    date: new Date(2022, 0, 31)
   },
   {
     title: "Buying Smart TV",
     amount: 8750000,
-    date: new Date(2022, 2, 31)
+    date: new Date(2022, 2, 12)
   },
   {
     title: "Buyin A New House",
     amount: 2500000000,
-    date: new Date(2022, 2, 31)
+    date: new Date(2021, 3, 9)
   },
   {
     title: "Holiday Expense",
     amount: 25000000,
-    date: new Date(2022, 2, 31)
+    date: new Date(2022, 10, 20)
   },
   ];
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      ></ExpenseItem>
+      <Expenses items={expense}/>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,10 @@
-import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate.js';
+import './ExpenseItem.css';
 
-function ExpenseItem(props) {
-    const month = props.date.toLocaleString('id-ID', {month : 'long'})
-    const year = props.date.toLocaleString('id-ID', {year : 'numeric'})
-    const day = props.date.toLocaleString('id-ID', {day : '2-digit'})
+function ExpenseItem(props) {   
   return (
     <div className='expense-item'>
-      <div>
-          <div>{month}</div>
-          <div>{year}</div>
-          <div>{day}</div>
-      </div>
+      <ExpenseDate date = {props.date}/>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>Rp. {props.amount}</div>
