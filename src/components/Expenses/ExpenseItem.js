c4c3a1e7f65a33df60a69ada28ec9e85350dfb58
Qmun14 <mamunramdhan@gmail.com>
2022-08-11 20:38:47 +0700

    Menyelesaikan Materi react state, working with events, menggunaakan konsep react hook -> useState() untuk mengUpgrade UI

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,25 @@
+import React, {useState} from 'react';
+
 import Card from '../UI/Card.js';
 import ExpenseDate from './ExpenseDate.js';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {   
+function ExpenseItem(props) {
+
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Updated by clicked!');
+    console.log(title);
+  }   
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date = {props.date}/>
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>Rp. {props.amount}</div>
+        <button onClick={clickHandler}>Change title</button>
       </div>
     </Card>
   );
