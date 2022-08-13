1ff05e3bb3281548c74da1aa5d4e68862bb39a95
Qmun14 <mamunramdhan@gmail.com>
2022-08-13 15:41:38 +0700

    Menyelesaikan Materi ( -Handling Form Submission- , -Adding Two-Way Binding- )

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -17,20 +17,35 @@ const ExpenseForm = () => {
         setEnteredDate(event.target.value);
     }
 
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+        console.log(expenseData);
+        // set value input clear : below & panggil value di element ke default value={defaultElement} kayak di bawah return jsx ini
+        // keuntungan nya dengan memanfaatkan state kita bisa menggunakan cara "adding Two - way binding" untuk memanipulasi value pada element nya di UI,
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    }
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new=expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler}/>
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0" step="0.1" onChange={amountChangeHandler}/>
+                    <input type="number" min="0" step="0.1" value={enteredAmount} onChange={amountChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+                    <input type="date" min="2019-01-01" max="2022-12-31" value={enteredDate}  onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense__actions">
