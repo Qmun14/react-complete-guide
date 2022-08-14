99a833e1ed440c77e4bce2317c1b184e2b5d8af7
Qmun14 <mamunramdhan@gmail.com>
2022-08-14 20:10:25 +0700

    Menyelesaikan Materi (-Understanding 'Keys' --> spesial function props yg disediakan oleh react.js agar tidak ada bug dalam merender list ke UI-)

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import Expenses from "./components/Expenses/Expenses.js";
 import NewExpense from "./components/NewExpense/NewExpense.js";
 
 const DUMMY_INIT_EXPENSES = [{
+  id: 'a1',
   title: "Car Insurance",
   amount: 1250000,
   date: new Date(2022, 0, 31)
 },
-{
+{ 
+  id: 'a2',
   title: "Buying Smart TV",
   amount: 8750000,
   date: new Date(2022, 2, 12)
 },
-{
+{ 
+  id: 'a3',
   title: "Buyin A New House",
   amount: 2500000000,
   date: new Date(2021, 3, 9)
 },
-{
+{ 
+  id: 'a4',
   title: "Holiday Expense",
   amount: 25000000,
   date: new Date(2022, 10, 20)
