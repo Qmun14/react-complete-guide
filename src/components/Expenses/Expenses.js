a60ff3d34a901fd2b7c9746c198a93a446e9ec88
Qmun14 <mamunramdhan@gmail.com>
2022-08-15 23:25:58 +0700

    Menyelesaikan Materi ( -Adding Conditional Returns Statements- ) ==> Memisahkan Antara logic conditional UI dengan content yang akan di render pada file yg berbeda agar lebih mudah di baca, tetapi ini pendekatan kondisional logic nya kurang oke dari yg sebelumnya, tergantung kebutuhan aja.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import Card from "../UI/Card.js";
-import ExpenseItem from "./ExpenseItem.js";
 
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter.js";
+import ExpensesList from "./ExpensesList.js";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -17,19 +17,6 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === filteredYear;
   })
 
-  let expensesContent = <p>No Expense Found!</p>
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))  
-  }
-
   return (
     <div>
       <Card className="expenses">
@@ -37,7 +24,7 @@ function Expenses(props) {
           selected={filteredYear}
           onTahunDipilih={filterChangeHandler}
         />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
   );
